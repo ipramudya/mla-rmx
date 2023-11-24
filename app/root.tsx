a089e0dc50b4417e14a49a1fc44697464768752b
ipramudya <ipramudya.dev@gmail.com>
2023-11-24 21:43:32 +0700

    init():  setup linter + formatter

--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,38 @@
+import "@fontsource-variable/source-sans-3/wght.css";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import globalStyles from "~/styles/global.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref
-    ? [
-        { rel: "stylesheet", href: cssBundleHref },
-        { rel: "icon", href: "/icon.svg", type: "image/svg" },
-      ]
-    : []),
+	...(cssBundleHref
+		? [
+				{ rel: "stylesheet", href: cssBundleHref },
+				{ rel: "stylesheet", href: globalStyles },
+				{ rel: "icon", href: "/icon.svg", type: "image/svg" },
+		  ]
+		: []),
 ];
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Mulailomba" },
-    { name: "description", content: "Mulailomba..." },
-  ];
+	return [{ title: "Mulailomba" }, { name: "description", content: "Mulailomba..." }];
 };
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
