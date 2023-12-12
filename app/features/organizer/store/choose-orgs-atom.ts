4ceccee1a9185fa5ff34bae93cfbf86a21251a72
ipramudya <ipramudya.dev@gmail.com>
2023-12-13 05:37:06 +0700

    fix(): remove dead code

--- app/features/organizer/store/choose-orgs-atom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-import { atomWithReset } from "jotai/utils";
-
-export const OrganizerItemPopupAtom = atomWithReset<{ id: string; val: boolean }>({
-	id: "",
-	val: false,
-});
-
-export const DashboardLoadingAtom = atom<boolean>(false);
