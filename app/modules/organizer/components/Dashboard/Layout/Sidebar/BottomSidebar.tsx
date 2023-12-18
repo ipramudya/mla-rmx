a19470b189f29dc782832d5748170ed4c57a52e8
ipramudya <ipramudya.dev@gmail.com>
2023-12-18 14:02:34 +0700

    feat(organizer): sidebar's bottom section

---
+++ app/modules/organizer/components/Dashboard/Layout/Sidebar/BottomSidebar.tsx
@@ -0,0 +1,42 @@
+import { Button, Stack } from "@mantine/core";
+import { Icon } from "app/components/Icon";
+
+export default function BottomSidebar() {
+	const handleSwitchToHomesite = () => {
+		console.log("switch to homesite");
+	};
+
+	const handleLogoutOrg = () => {
+		console.log("logout orgs");
+	};
+
+	return (
+		<Stack p={12} style={{ borderRadius: "10px" }} bg="white">
+			<Button
+				variant="subtle"
+				color="var(--mantine-color-dark-3)"
+				leftSection={<Icon.Back size={20} />}
+				styles={{
+					inner: {
+						justifyContent: "start",
+					},
+				}}
+				onClick={handleSwitchToHomesite}
+			>
+				Beralih ke Halaman Utama
+			</Button>
+			<Button
+				variant="subtle"
+				color="red"
+				leftSection={<Icon.Logout size={20} />}
+				styles={{
+					section: { transform: "rotate(180deg)" },
+					inner: { justifyContent: "start" },
+				}}
+				onClick={handleLogoutOrg}
+			>
+				Keluar Organizer
+			</Button>
+		</Stack>
+	);
+}
--- app/modules/organizer/components/Dashboard/Layout/Sidebar/Sidebar.tsx
+++ app/modules/organizer/components/Dashboard/Layout/Sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
 import { Flex } from "@mantine/core";
+import BottomSidebar from "./BottomSidebar";
 import OrganizerChannels from "./OrganizerChannels";
 
 export default function Sidebar() {
 	return (
-		<Flex component="aside" gap={8}>
-			<OrganizerChannels />
+		<Flex component="aside" gap={8} direction="column">
+			<Flex gap={8} style={{ flexGrow: 1 }}>
+				<OrganizerChannels />
 
-			{/* sidenavs */}
+				{/* sidenavs */}
+			</Flex>
+
+			{/* bottom nav */}
+			<BottomSidebar />
 		</Flex>
 	);
 }
